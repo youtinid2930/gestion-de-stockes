3bd307dcc9cf5e5495aaad6048776cedabc70a8c
youtinid2930 <tinidyoussef2023@gmail.com>
2024-09-23 17:48:21 +0100

    this changes before the cherry pick from anwarbranche

--- public/js/custom.js
+++ public/js/custom.js
@@ -3,10 +3,11 @@ document.addEventListener("DOMContentLoaded", function() {
     let sidebar = document.querySelector(".sidebar");
     let sidebarBtn = document.querySelector(".sidebar-button");
     let homeSection = document.querySelector(".home-section");
-
+    let searchBox = document.querySelector("#searchBox");
     sidebarBtn.onclick = function() {
         sidebar.classList.toggle("active");
         homeSection.classList.toggle("active");
+        searchBox.classList.toggle("active");
         if (sidebar.classList.contains("active")) {
             sidebarBtn.querySelector('i').classList.replace("bx-menu", "bx-menu-alt-right");
         } else {
